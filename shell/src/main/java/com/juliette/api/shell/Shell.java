e651c980dbe2e44582a321995fe43a115e225c42
silver <silver@silvers-MacBook-Pro.local>
2017-12-10 12:38:13 -0500

    https://github.com/julietteQuebec/api/issues/1
    connection to server by command shell

---
+++ shell/src/main/java/com/juliette/api/shell/Shell.java
@@ -0,0 +1,68 @@
+package com.juliette.api.shell;
+
+
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Shell {
+
+    public static void main(String args[]) {
+
+        StringBuffer output =  new StringBuffer();
+        try {
+
+            JSch jsch = new JSch();
+
+            jsch.addIdentity("");
+            jsch.setConfig("StrictHostKeyChecking", "no");
+            String user = "";
+            String host = "";
+
+            Session session=jsch.getSession(user, host, 22);
+            session.connect();
+
+            String command = "ls -ltr";
+
+            ChannelExec exec = (ChannelExec)session.openChannel("exec");
+             exec.setCommand(command);
+            exec.connect();
+
+            BufferedReader readerRemote = new BufferedReader(new InputStreamReader(exec.getInputStream()));
+
+            String lineRemote;
+
+            while((lineRemote = readerRemote.readLine()) != null) {
+                output.append(lineRemote + "\n");
+            }
+
+            exec.disconnect();
+            session.disconnect();
+
+
+
+
+            Process process =  Runtime.getRuntime().exec("ls -ltr");
+            process.waitFor();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+
+            while((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+        } catch(IOException | InterruptedException uu) {
+           uu.printStackTrace();
+        } catch(JSchException uu) {
+            uu.printStackTrace();
+        }
+        System.out.printf("output>>" + output.toString());
+
+    }
+}
---
+++ shell/src/main/java/com/juliette/api/shell/ShellApplication.java
@@ -0,0 +1,12 @@
+package com.juliette.api.shell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShellApplication.class, args);
+	}
+}
